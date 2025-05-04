3a43605b64c0426eb2d5ede8b6ededa8584410bd
hads17 <bradleysev@gmail.com>
2025-05-03 21:12:28 -0400

    352025 - added Split_nodes_delimiter

--- src/modules/utilities.py
+++ src/modules/utilities.py
@@ -16,3 +16,15 @@ def text_node_to_html_node(text_node):
         return LeafNode('img', '', None, {"src":text_node.url, "alt":text_node.text})
     else:
         raise ValueError("Invalid Text Type")
+    
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    result = []
+    for node in old_nodes:
+        split_node_text = []
+        if node.text_type == TextType.TEXT:
+            result.append(node)
+        if text_type == TextType.BOLD:
+            split_node_text = node.text.split(delimiter)
+            if len(split_node_text) < 3 and len(split_node_text) % 2 == 0:
+                raise ValueError("No Matching closing delimiter found")
+            
